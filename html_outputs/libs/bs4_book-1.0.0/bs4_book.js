01177c8b758bf6079519f03c03b0f16be3f9a1f2
jcfernandezm <juanc.fernandez.merino@gmail.com>
2023-01-17 10:18:49 +0100

    Libro completo una vez renderizado. Se han modificado también /html/outputs/libs/bs4_book-1.0.0 en las lineas 110 y 124 para que aparezca traducido "Copiar en el portapapeles" y "copiado"

--- html_outputs/libs/bs4_book-1.0.0/bs4_book.js
+++ html_outputs/libs/bs4_book-1.0.0/bs4_book.js
@@ -107,7 +107,7 @@ function changeTooltipMessage(element, msg) {
 $(document).ready(function() {
   if(ClipboardJS.isSupported()) {
     // Insert copy buttons
-    var copyButton = "<button type='button' class='btn btn-copy' title='Copy to clipboard' aria-label='Copy to clipboard' data-toggle='popover' data-placement='top' data-trigger='hover'><i class='bi'></i></button>";
+    var copyButton = "<button type='button' class='btn btn-copy' title='Copiar en el portapapeles' aria-label='Copy to clipboard' data-toggle='popover' data-placement='top' data-trigger='hover'><i class='bi'></i></button>";
     $(copyButton).appendTo("div.sourceCode");
     // Initialize tooltips:
     $('.btn-copy').tooltip({container: 'body', boundary: 'window'});
@@ -121,7 +121,7 @@ $(document).ready(function() {
 
     clipboard.on('success', function(e) {
       const btn = e.trigger;
-      changeTooltipMessage(btn, 'Copied!');
+      changeTooltipMessage(btn, 'Copiado!');
       btn.classList.add('btn-copy-checked');
       setTimeout(function() {
         btn.classList.remove('btn-copy-checked');
